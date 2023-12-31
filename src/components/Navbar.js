9cac21d3cb22d27116b0c6d65ef020c09492489c
Raul <raulsmarius@yahoo.com>
2023-12-31 14:57:42 +0200

    Added Different Animation to Sidebar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,6 @@
 import { useState } from 'react'
 import { Link } from 'react-router-dom'
 import { XMarkIcon, Bars3Icon } from '@heroicons/react/20/solid'
-import { Transition } from '@headlessui/react'
 const Navbar = () => {
   const [sidebar, setSidebar] = useState(false)
   const showSidebar = () => setSidebar(!sidebar)
@@ -33,33 +32,32 @@ const Navbar = () => {
           <Bars3Icon onClick={showSidebar} className="text-pink-300 h-8 w-8" />
         </Link>
       </div>
-      <Transition
-        show={sidebar}
-        enter="ease-out duration-300"
-        enterFrom="opacity-0"
-        enterTo="opacity-100"
-        leave="ease-in duration-300"
-        leaveFrom="opacity-100"
-        leaveTo="opacity-0"
+      <nav
+        className={`bg-slate-800 w-[300px] h-full rounded-xl flex flex-col items-start fixed inset-y-0 ${
+          !!sidebar ? 'left-0' : 'left-[-100%]'
+        } duration-500`}
       >
-        <nav className="bg-slate-800 w-[300px] h-full flex flex-col items-start fixed inset-y-0 left-0">
-          <div className="flex w-full justify-end p-4 m-2">
-            <XMarkIcon
-              onClick={showSidebar}
-              className="h-8 w-8 text-pink-300 cursor-pointer"
-            />
-          </div>
-          <ul className="flex flex-col items-start ml-12 p-6">
-            {sidebarItems.map((item, i) => {
-              return (
-                <li key={i} className="text-pink-300 my-2 p-2">
-                  <Link to={item.path}>{item.title}</Link>
-                </li>
-              )
-            })}
-          </ul>
-        </nav>
-      </Transition>
+        <div className="flex w-full justify-end p-4 m-2">
+          <XMarkIcon
+            onClick={showSidebar}
+            className="h-8 w-8 text-pink-200 cursor-pointer hover:text-pink-300 hover:scale-110 duration-500"
+          />
+        </div>
+        <ul className="flex flex-col items-start ml-12 p-6">
+          {sidebarItems.map((item, i) => {
+            return (
+              <li
+                key={i}
+                className="text-pink-200 my-2 p-2 hover:text-pink-300 hover:scale-110 duration-500"
+              >
+                <Link to={item.path} onClick={showSidebar}>
+                  {item.title}
+                </Link>
+              </li>
+            )
+          })}
+        </ul>
+      </nav>
     </div>
   )
 }
